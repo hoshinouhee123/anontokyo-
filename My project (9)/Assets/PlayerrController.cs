3e7564ececee2f9cfc685f972537f2f49b01c3c8
hoshinouhee123 <mna209459@gmail.com>
2026-03-18 16:05:24 +0900

    Add : 플레이어 애니메이션, 점프, 맵 이동 트리거 추가

--- My project (9)/Assets/PlayerrController.cs
+++ My project (9)/Assets/PlayerrController.cs
@@ -1,14 +1,46 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerrController : MonoBehaviour
 {
     public Vector2 moveInput;
+    public float jumpForce = 7f;
     public float moveSpeed = 130.0f;
+    private Animator Circle;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Circle = GetComponent<Animator>();
+        Circle.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene("PlayScene_" + collision.name);
+        }
     }
 
     // Update is called once per frame
@@ -21,6 +53,15 @@ public class PlayerrController : MonoBehaviour
         else if(moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(moveInput.magnitude > 0)
+        {
+            Circle.SetBool("move", true);
+        }
+        else
+        {
+            Circle.SetBool("move", false);
         }
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
