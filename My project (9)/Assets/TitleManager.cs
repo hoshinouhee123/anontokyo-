eca01baff5ed0996a85108cb802dc531d17d91bd
hoshinouhee123 <mna209459@gmail.com>
2026-04-01 09:14:53 +0900

    Add:플레이어 기본 움직임, 메인화면 ui

---
+++ My project (9)/Assets/TitleManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // 여기에 아까 만든 도움말 패널(HelpPanel)을 연결할 겁니다.
+    public GameObject helpPanel;
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("PlayScene_Door1");
+    }
+
+    // 도움말 켜기 함수 (버튼 누르면 실행)
+    public void OpenHelp()
+    {
+        helpPanel.SetActive(true); // 팝업창을 보이게(true) 함
+    }
+
+    // 도움말 끄기 함수 (닫기 버튼 누르면 실행)
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false); // 팝업창을 안 보이게(false) 함
+    }
+}
